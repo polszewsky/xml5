21818fe46b9f90370238579ab6d336331c4a307a
polszewsky <32812297+polszewsky@users.noreply.github.com>
2018-06-19 03:06:01 +0200

    XML TRANSFORM <3

--- src/main/java/xmlproject/transform/PDFTransform.java
+++ src/main/java/xmlproject/transform/PDFTransform.java
@@ -31,42 +31,19 @@ public class PDFTransform {
 		// TXT
 		//*******
 		
-	//	TransformerFactory tFactory = null;
-	//	Transformer transformer = null;
-	//	 /* Create a TransformerFactory object */           
-	//    tFactory = TransformerFactory.newInstance();
-	//    /* Get the incoming XSLT file */
-	//    try {
-	//		transformer = tFactory.newTransformer(new StreamSource("xml\\zadanie4_toTXT.xslt"));
-	//	} catch (TransformerConfigurationException e) {
-	//		// TODO Auto-generated catch block
-	//		e.printStackTrace();
-	//	}
-	//    /* Get the XML file and apply the XSLT transformation to convert to HTML */
-	//    try {
-	//		transformer.transform(new StreamSource("xml\\zadanie4.xml"),new StreamResult(new FileOutputStream("xml\\output.txt")));
-	//	} catch (FileNotFoundException e) {
-	//		// TODO Auto-generated catch block
-	//		e.printStackTrace();
-	//	} catch (TransformerException e) {
-	//		// TODO Auto-generated catch block
-	//		e.printStackTrace();
-	//	}
-	
-	
 		TransformerFactory tFactory = null;
 		Transformer transformer = null;
 		 /* Create a TransformerFactory object */           
-        tFactory = TransformerFactory.newInstance();
-        /* Get the incoming XSLT file */
-        try {
-			transformer = tFactory.newTransformer(new StreamSource("xml\\zadanie4_toPDF.xslt"));
+	    tFactory = TransformerFactory.newInstance();
+	    /* Get the incoming XSLT file */
+	    try {
+			transformer = tFactory.newTransformer(new StreamSource("xml\\winestore_xhtml.xsl"));
 		} catch (TransformerConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        /* Get the XML file and apply the XSLT transformation to convert to HTML */
-        try {
+	    /* Get the XML file and apply the XSLT transformation to convert to HTML */
+	    try {
 			transformer.transform(new StreamSource("xml\\zadanie4.xml"),new StreamResult(new FileOutputStream("xml\\output.html")));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -75,17 +52,40 @@ public class PDFTransform {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-        String File_To_Convert = "xml\\output.html";
-        String url = new File(File_To_Convert).toURI().toURL().toString();
-        System.out.println(""+url);
-        String HTML_TO_PDF = "xml\\ConvertedFile.pdf";
-        OutputStream os = new FileOutputStream(HTML_TO_PDF);       
-        ITextRenderer renderer = new ITextRenderer();
-        renderer.setDocument(url);      
-        renderer.layout();
-        renderer.createPDF(os);        
-        os.close();
+	
+	
+//		TransformerFactory tFactory = null;
+//		Transformer transformer = null;
+//		 /* Create a TransformerFactory object */           
+//        tFactory = TransformerFactory.newInstance();
+//        /* Get the incoming XSLT file */
+//        try {
+//			transformer = tFactory.newTransformer(new StreamSource("xml\\zadanie4_toPDF.xslt"));
+//		} catch (TransformerConfigurationException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        /* Get the XML file and apply the XSLT transformation to convert to HTML */
+//        try {
+//			transformer.transform(new StreamSource("xml\\zadanie4.xml"),new StreamResult(new FileOutputStream("xml\\output.html")));
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (TransformerException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//        String File_To_Convert = "xml\\output.html";
+//        String url = new File(File_To_Convert).toURI().toURL().toString();
+//        System.out.println(""+url);
+//        String HTML_TO_PDF = "xml\\ConvertedFile.pdf";
+//        OutputStream os = new FileOutputStream(HTML_TO_PDF);       
+//        ITextRenderer renderer = new ITextRenderer();
+//        renderer.setDocument(url);      
+//        renderer.layout();
+//        renderer.createPDF(os);        
+//        os.close();
         
 		
 	}
--- xml/winestore_xhtml.xsl
+++ xml/winestore_xhtml.xsl
@@ -4,7 +4,7 @@
 
 <xsl:stylesheet version="3.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 
-<xsl:output  method="xhtml"    encoding="UTF-8"     indent="yes"/>
+<xsl:output  method="html"    encoding="UTF-8"     indent="yes"/>
 
 <xsl:template match="winestore">
 
